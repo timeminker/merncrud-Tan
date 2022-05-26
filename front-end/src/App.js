07bd3422408809f715c9cc78286d6e568e0a76b9
Tan Le <tanle@Tans-MBP.hsd1.fl.comcast.net>
2022-05-26 16:51:17 -0400

    MVP CRUD

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,22 @@ import './App.css';
 import {useState, useEffect} from 'react'
 import axios from 'axios'
 
+
+
+
+
+// <form onSubmit={(event) => {
+//   handleUpdateEntry(event,book)
+// }}>
+// Name: <input type="text" onChange={handleNewNameChange}/><br/>
+// Category: <input type="text" onChange={handleNewCategoryChange}/><br/>
+// Author: <input type="text" onChange={handleNewAuthorChange}/><br/>
+// Image: <input type="url" onChange={handleNewImageChange}/><br/>
+// Booked: <input type="checkbox" onChange={handleNewBookedChange}/><br/>
+// <input type="submit" value="Edit Book"/>
+// </form>
+
+
 const App = () => {
 
   const [library, setLibrary] = useState([])
@@ -11,6 +27,25 @@ const App = () => {
   const [newAuthor, setNewAuthor] = useState('')
   const [newImage, setNewImage] = useState('')
   const [newBooked, setNewBooked] = useState(false)
+  const [show, setShow] = useState(false)
+  const [editLibrary, setEditLibrary] = useState([])
+  const [showEdit, setShowEdit] = useState(false)
+
+  const handleShowEdit = () => {
+    setShowEdit(!showEdit)
+  }
+
+  const reveal = () => {
+      setShow(!show)
+
+  }
+
+  const reset = () => {
+    setNewName('')
+    setNewAuthor('')
+    setNewImage('')
+    setNewBooked(false)
+  }
 
   const newBookSubmit = (event) => {
     event.preventDefault()
@@ -20,10 +55,12 @@ const App = () => {
       category: newCategory,
       author: newAuthor,
       image: newImage,
-      booked: newBooked
+      booked: newBooked,
+      showEdit:false
     }).then(() => {
       axios.get('http://localhost:3000/books').then((response) => {
         setLibrary(response.data)
+        reset()
       })
     })
   }
@@ -33,6 +70,11 @@ const App = () => {
       setLibrary(response.data)
     })
   }, [])
+
+  const refreshPage = (event) => {
+    event.preventDefault()
+    // window.location.reload(false)
+  }
 
   const handleNewNameChange = (event) => {
     setNewName(event.target.value)
@@ -51,34 +93,101 @@ const App = () => {
   }
 
 
+
+  const handleDeleteEntry = (bookData) => {
+    axios.delete(`http://localhost:3000/books/${bookData._id}`).then(() => {
+      axios.get('http://localhost:3000/books').then((response) => {
+        setLibrary(response.data)
+      })
+    })
+  }
+
+  const handleUpdateEntry = (event, bookData) => {
+    event.preventDefault()
+    setEditLibrary(library)
+    axios.put(`http://localhost:3000/books/${bookData._id}`, {
+      name: newName,
+      category: newCategory,
+      author: newAuthor,
+      image: newImage,
+      booked: newBooked,
+      showEdit:false
+    }).then(() => {
+      axios.get('http://localhost:3000/books').then((response) => {
+        setLibrary(response.data)
+      })
+    })
+  }
+
+
+
   return (
-    <>
-      <h1>Library</h1>
+    <div className='container'>
+      <div className='library'>
+        <h1>Library</h1>
+      </div>
+      <header>
       <h2>Add New Book</h2>
       <form onSubmit={newBookSubmit}>
-        Name: <input type="text" onChange={handleNewNameChange}/><br/>
-        Category: <input type="text" onChange={handleNewCategoryChange}/><br/>
+        Name: <input type="text" onChange={handleNewNameChange} required/><br/>
+        Category: <select name="category" onChange={handleNewCategoryChange}>
+                    <option value='fiction'>Fiction</option>
+                    <option value='fantasy'>Fantasy</option>
+                    <option value='non-fiction'>Non-fiction</option>
+                    <option value='biography'>Biography</option>
+                  </select><br/>
         Author: <input type="text" onChange={handleNewAuthorChange}/><br/>
         Image: <input type="url" onChange={handleNewImageChange}/><br/>
         Booked: <input type="checkbox" onChange={handleNewBookedChange}/><br/>
         <input type="submit" value="Add Book"/>
       </form>
+      </header>
+      <div>
+        <button>Fantasy</button><br/>
+        Fiction<input type="checkbox" /><br/>
+        Non-fiction<input type="checkbox" /><br/>
+        Biography<input type="checkbox" /><br/>
+      </div>
       <div>
         <ul>
-          {library.map((book) => {
+          {library.map((book, index) => {
             return (
               <li key={book._id}>
-              {book.name}<br/>
+              Name: {book.name}<br/>
+              Author: {book.author}<br/>
+              Category: {book.category}<br/>
+              <img src={book.image}/>
+              {book.booked ? <p>Is Reserved</p> : <p>Available</p>}<br/>
+              {show && book.category == 'fiction' ?
+              <div>{book.name}<br/>
               {book.author}<br/>
               {book.category}<br/>
-              <img src={book.image}/>
-              {book.booked ? <p>Is Reserved</p> : <p>Available</p>}
+              <img src={book.image}/></div> : null }
+              { showEdit ?
+                <form onSubmit={(event) => {
+                handleUpdateEntry(event,book)
+              }}>
+              Name: <input type="text" onChange={handleNewNameChange} required/><br/>
+              Category: <select name="category" onChange={handleNewCategoryChange}>
+                          <option value='fiction'>Fiction</option>
+                          <option value='fantasy'>Fantasy</option>
+                          <option value='non-fiction'>Non-fiction</option>
+                          <option value='biography'>Biography</option>
+                        </select><br/>
+              Author: <input type="text" onChange={handleNewAuthorChange}/><br/>
+              Image: <input type="url" onChange={handleNewImageChange}/><br/>
+              Booked: <input type="checkbox" onChange={handleNewBookedChange}/><br/>
+              <input type="submit" value="Edit Book"/>
+              </form> : <button onClick={(event) => {handleShowEdit()}}>Click to edit</button> }
+              <button onClick={(event) => {
+                handleDeleteEntry(book)
+              }}>Delete</button>
               </li>
             )
           })}
         </ul>
       </div>
-    </>
+    </div>
   )
 }
 
